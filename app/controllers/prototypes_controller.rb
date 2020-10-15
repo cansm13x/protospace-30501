74e57151474c9eb6218ad6f5728953555b1e05a9
cansm13x <cansm.13x@gmail.com>
2020-10-15 17:10:24 +0900

    変更修正１（10.15）

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,7 +2,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, only: [:edit, :destroy]
 
   def index
-    @prototypes = Prototype.all
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
